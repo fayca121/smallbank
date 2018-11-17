211186abc97f13183d6e295ec50cd2d518eee2c6
Bououza Faycal <bz.faycal@gmail.com>
2018-11-17 13:33:39 +0100

    show account information

--- src/main/java/bz/faycal/smallbank/controller/AccountController.java
+++ src/main/java/bz/faycal/smallbank/controller/AccountController.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class AccountController {
@@ -22,8 +21,11 @@ public class AccountController {
     public String index(){
         return "accounts";
     }
+
+
     @GetMapping("/searchAccount")
-    public String searchAccount(Model model, @PathVariable("accountcode") String accountCode){
+    public String searchAccount(Model model, String accountCode){
+        model.addAttribute("accountCode",accountCode);
         try {
             Account account = bankService.checkAccount(accountCode);
             model.addAttribute("account",account);
