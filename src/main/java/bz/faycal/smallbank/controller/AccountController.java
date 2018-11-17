3d10a0a425ccc5f0e5fca415b8b219520b054cf5
Bououza Faycal <bz.faycal@gmail.com>
2018-11-17 14:02:57 +0100

    add list of operations

--- src/main/java/bz/faycal/smallbank/controller/AccountController.java
+++ src/main/java/bz/faycal/smallbank/controller/AccountController.java
@@ -1,8 +1,10 @@
 package bz.faycal.smallbank.controller;
 
 import bz.faycal.smallbank.entities.Account;
+import bz.faycal.smallbank.entities.Operation;
 import bz.faycal.smallbank.service.IBankService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -28,6 +30,8 @@ public class AccountController {
         model.addAttribute("accountCode",accountCode);
         try {
             Account account = bankService.checkAccount(accountCode);
+            Page<Operation> operations=bankService.operationsList(accountCode,0,4);
+            model.addAttribute("operations",operations.getContent());
             model.addAttribute("account",account);
         }catch (Exception e){
             model.addAttribute("exception",e);
