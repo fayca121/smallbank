2c08a55fb1170293f1ef834681ee44e347d94a45
Bououza Faycal <bz.faycal@gmail.com>
2018-11-17 12:56:48 +0100

    add search implementation

--- src/main/java/bz/faycal/smallbank/controller/AccountController.java
+++ src/main/java/bz/faycal/smallbank/controller/AccountController.java
@@ -1,9 +1,12 @@
 package bz.faycal.smallbank.controller;
 
+import bz.faycal.smallbank.entities.Account;
 import bz.faycal.smallbank.service.IBankService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class AccountController {
@@ -17,6 +20,17 @@ public class AccountController {
 
     @GetMapping("/operations")
     public String index(){
+        return "accounts";
+    }
+    @GetMapping("/searchAccount")
+    public String searchAccount(Model model, @PathVariable("accountcode") String accountCode){
+        try {
+            Account account = bankService.checkAccount(accountCode);
+            model.addAttribute("account",account);
+        }catch (Exception e){
+            model.addAttribute("exception",e);
+        }
+
         return "accounts";
     }
 }
