a36f66549cfdaeb2a8ecf9418bd109d361fffea2
Bououza Faycal <bz.faycal@gmail.com>
2018-11-19 21:42:17 +0100

    add spring security framework

---
+++ src/main/java/bz/faycal/smallbank/configuration/SecurityConfig.java
@@ -0,0 +1,44 @@
+package bz.faycal.smallbank.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        //return new BCryptPasswordEncoder();
+        return NoOpPasswordEncoder.getInstance();
+    }
+
+    @Override
+    public void configure(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception{
+        /*authenticationManagerBuilder.userDetailsService(null)
+                .passwordEncoder(new BCryptPasswordEncoder());*/
+        authenticationManagerBuilder.inMemoryAuthentication()
+                .withUser("admin").password("1234").roles("ADMIN","USER");
+        authenticationManagerBuilder.inMemoryAuthentication()
+                .withUser("user").password("1234").roles("USER");
+    }
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+
+        http.authorizeRequests()
+                .antMatchers("/operations","/searchAccount")
+                .hasRole("USER")
+                .antMatchers("/createOperation").hasRole("ADMIN")
+                .and()
+                .formLogin();
+    }
+}
