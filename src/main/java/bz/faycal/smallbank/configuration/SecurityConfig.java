c99e26205f7ab1b1576ec8c1a262258b329d4f91
Bououza Faycal <bz.faycal@gmail.com>
2018-11-19 23:22:17 +0100

    Add extra thymeleaf security lib for using extra tags

--- src/main/java/bz/faycal/smallbank/configuration/SecurityConfig.java
+++ src/main/java/bz/faycal/smallbank/configuration/SecurityConfig.java
@@ -2,6 +2,7 @@ package bz.faycal.smallbank.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
@@ -33,11 +34,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     public void configure(HttpSecurity http) throws Exception {
-
         http.authorizeRequests()
                 .antMatchers("/operations","/searchAccount")
                 .hasRole("USER")
-                .antMatchers("/createOperation").hasRole("ADMIN")
+                .antMatchers("/createOperation")
+                .hasRole("ADMIN")
                 .and()
                 .formLogin();
     }
