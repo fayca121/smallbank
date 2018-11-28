18c8a20c205695ed04381009bc4a789f447b5d66
Bououza Faycal <bz.faycal@gmail.com>
2018-11-28 20:04:47 +0100

    add client list

--- src/main/java/bz/faycal/smallbank/controller/CustomerController.java
+++ src/main/java/bz/faycal/smallbank/controller/CustomerController.java
@@ -3,13 +3,21 @@ package bz.faycal.smallbank.controller;
 import bz.faycal.smallbank.entities.Client;
 import bz.faycal.smallbank.repository.ClientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Optional;
 
 @Controller
 public class CustomerController {
+
+    private static final int INITIAL_PAGE = 0;
+    private static final int INITIAL_PAGE_SIZE = 5;
 
     private ClientRepository clientRepository;
 
@@ -19,6 +27,15 @@ public class CustomerController {
     }
 
     @GetMapping("/customers")
+    public String index(Model uiModel,@RequestParam("page") Optional<Integer> page){
+        Page<Client> clients= clientRepository.findAll(PageRequest.of(page.orElse(INITIAL_PAGE),INITIAL_PAGE_SIZE));
+        uiModel.addAttribute("page",page.orElse(INITIAL_PAGE));
+        uiModel.addAttribute("clients",clients.getContent());
+        uiModel.addAttribute("totalPages",clients.getTotalPages());
+        return "clients";
+    }
+
+    @GetMapping("/client")
     public String index(){
         return "client";
     }
